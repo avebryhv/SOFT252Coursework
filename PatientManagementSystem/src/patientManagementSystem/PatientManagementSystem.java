ba1a6c545ab20738fbcff32ab83b7febb0872dc7
avebryhv <33032139+avebryhv@users.noreply.github.com>
2019-01-17 00:38:11 +0000

    Reorganised library

--- PatientManagementSystem/src/patientManagementSystem/PatientManagementSystem.java
+++ PatientManagementSystem/src/patientManagementSystem/PatientManagementSystem.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package patientmanagementsystem;
+package patientManagementSystem;
 
 import GUI.MainGUI;
 import Serialiser.Serialiser;
+import patientmanagementsystem.MedicineManager;
+import patientmanagementsystem.UserManagement;
 
 /**
  *Main Method
